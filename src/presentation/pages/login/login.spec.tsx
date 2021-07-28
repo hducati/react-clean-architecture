68c6531a0d432561e6264dc0539be597b0be4262
Henrique Ducati <henrique.dcti@gmail.com>
2021-07-28 15:38:36 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Login from './login'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+
+  return {
+    sut
+  }
+}
 
 describe('Login component', () => {
   test('should start with initial state', () => {
-    const { getByTestId } = render(<Login />)
-    const errorWrap = getByTestId('error-wrap')
-    const submitButton = getByTestId('submit') as HTMLButtonElement
-    const emailStatus = getByTestId('email-status')
-    const passwordStatus = getByTestId('password-status')
+    const { sut } = makeSut()
+    const errorWrap = sut.getByTestId('error-wrap')
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+    const emailStatus = sut.getByTestId('email-status')
+    const passwordStatus = sut.getByTestId('password-status')
 
     expect(errorWrap.childElementCount).toBe(0)
     expect(submitButton.disabled).toBe(true)
