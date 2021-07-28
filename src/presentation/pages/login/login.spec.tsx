e1b65a249d2aba5417d17ad65a0e92b2ef3b0cd8
Henrique Ducati <henrique.dcti@gmail.com>
2021-07-28 15:59:22 -0300

    test: ensure Login calls Validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -56,4 +56,15 @@ describe('Login component', () => {
       email: 'any_email'
     })
   })
+
+  test('should call Validation with correct password', () => {
+    const { sut, validationSpy } = makeSut()
+    const passwordInput = sut.getByTestId('password')
+
+    fireEvent.input(passwordInput, { target: { value: 'any_password'}})
+
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
+    })
+  })
 })
