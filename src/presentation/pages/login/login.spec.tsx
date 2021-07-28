78387bae7fa7a98367b574ef60af67d7c34d5f27
Henrique Ducati <henrique.dcti@gmail.com>
2021-07-28 14:30:25 -0300

    fix: move global styles to index

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react'
+import Login from './login'
+
+describe('Login component', () => {
+  test('', () => {
+    render(<Login />)
+  })
+})
