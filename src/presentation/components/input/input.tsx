02be1f6963760f71407ba3741bf83ac8ec2444fc
Henrique Ducati <henrique.dcti@gmail.com>
2021-07-28 17:21:14 -0300

    feat: ensure Login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -19,11 +19,11 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🔵'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo certo!'
   }
   
   return (
