07c84147ed9386f32f0836c345f562737b9be628
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-03 12:51:14 -0300

    feat: ensure Login add accessToken to localStorage

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -35,13 +35,13 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
       if (state.isLoading || state.emailError || state.passwordError) {
         return
       }
-  
       setState({ ...state, isLoading: true})
-  
-      await authentication.auth({
+      const account = await authentication.auth({
         email: state.email,
         password: state.password
       })
+  
+      localStorage.setItem('accessToken', account.accessToken)
     } catch(error) {
       setState({
         ...state, 
