0568334a200811f58cb83b440b0844ff8732e998
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-04 17:31:44 -0300

    test: ensure ValidationComposite return falsy

--- src/validation/validation-composite/validation-composite.spec.ts
+++ src/validation/validation-composite/validation-composite.spec.ts
@@ -1,15 +1,16 @@
 import { ValidationComposite } from "@/validation/validation-composite/validation-composite"
 import { FieldValidationSpy } from "@/validation/test/mock-field-validation";
+import faker from "faker"
 
 type SutTypes = {
   sut: ValidationComposite
   fieldValidationsSpy: FieldValidationSpy[]
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (fieldName: string): SutTypes => {
   const fieldValidationsSpy = [
-    new FieldValidationSpy('any_field'),
-    new FieldValidationSpy('any_field')
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
   ]
 
   const sut = new ValidationComposite(
@@ -24,12 +25,23 @@ const makeSut = (): SutTypes => {
 
 describe('EmailValidation', () => {
   test('should return error if any validation fails', () => {
-    const { sut, fieldValidationsSpy }= makeSut()
-    fieldValidationsSpy[0].error = new Error('first_error_message')
-    fieldValidationsSpy[1].error = new Error('second_error_message')
+    const fieldName = faker.database.column()
+    const { sut, fieldValidationsSpy }= makeSut(fieldName)
+    const errorMessage = faker.random.words()
+    fieldValidationsSpy[0].error = new Error(errorMessage)
+    fieldValidationsSpy[1].error = new Error(faker.random.words())
 
-    const error = sut.validate('any_field', 'any_value')
+    const error = sut.validate(fieldName, faker.random.word())
 
-    expect(error).toBe('first_error_message')
+    expect(error).toBe(errorMessage)
+  })
+
+  test('should return error if any validation fails', () => {
+    const fieldName = faker.database.column()
+    const { sut }= makeSut(fieldName)
+
+    const error = sut.validate(fieldName, faker.random.word())
+
+    expect(error).toBeFalsy()
   })
 })
