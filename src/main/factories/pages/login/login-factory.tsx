1b0b06cddc71619b885ab26b752d3a7a800cbc31
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-11 11:16:35 -0300

    feat: change Login composition to use factories

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Login } from '@/presentation/pages'
 import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
 import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
+import { makeLocalSaveAccessToken } from '../../usecases/save-access-token/local-save-access-token-factory'
 
 export const makeLogin: React.FC = () => {
   return (
     <Login
       authentication={makeRemoteAuthentication()}
       validation={makeLoginValidation()}
+      saveAccessToken={makeLocalSaveAccessToken()}
     />
   )
 }
