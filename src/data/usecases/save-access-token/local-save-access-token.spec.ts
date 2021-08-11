ffcfd1a746883e0f845be798fec4ea10ea2519d0
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-11 09:27:34 -0300

    refactor: rename SetStorageSpy to SetStorageMock

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -1,29 +1,29 @@
 import { LocalSaveAccessToken } from './local-save-access-token'
-import { SetStorageSpy } from '@/data/test/mock-storage'
+import { SetStorageMock } from '@/data/test/mock-storage'
 import faker from 'faker'
 
 type SutTypes = {
   sut: LocalSaveAccessToken
-  setStorageSpy: SetStorageSpy
+  setStorageMock: SetStorageMock
 }
 
 const makeSut = (): SutTypes => {
-  const setStorageSpy = new SetStorageSpy()
-  const sut = new LocalSaveAccessToken(setStorageSpy)
+  const setStorageMock = new SetStorageMock()
+  const sut = new LocalSaveAccessToken(setStorageMock)
 
   return {
-    setStorageSpy,
+    setStorageMock,
     sut
   }
 }
 
 describe('LocalSaveAccessToken', () => {
   test('should call SetStorage with correct value', async () => {
-    const { setStorageSpy, sut } = makeSut()
+    const { setStorageMock, sut } = makeSut()
     const accessToken = faker.datatype.uuid()
     await sut.save(accessToken)
 
-    expect(setStorageSpy.key).toBe('accessToken')
-    expect(setStorageSpy.value).toBe(accessToken)
+    expect(setStorageMock.key).toBe('accessToken')
+    expect(setStorageMock.value).toBe(accessToken)
   })
 })
