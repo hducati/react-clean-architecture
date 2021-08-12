d7d20d5ea8a9452233a5227b50ad1df31ba12e0f
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-12 10:03:11 -0300

    feat: ensure SignUp works properly with Validation

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Styles from './signup-styles.scss'
-import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
-import { Link } from 'react-router-dom'
+import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
+import { Validation } from '@/presentation/protocols/validation'
 
-const SignUp: React.FC = () => {
+type Props = {
+  validation: Validation
+}
+
+const SignUp: React.FC<Props> = ({ validation }: Props) => {
+  const [state, setState] = useState({
+    isLoading: false,
+    name: '',
+    nameError: '',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório',
+    passwordConfirmationError: 'Campo obrigatório',
+    mainError: ''
+  })
+
+  useEffect(() => {
+    setState({
+      ...state,
+      nameError: validation.validate('name', state.name)
+    })
+  }, [state.name])
+
   return (
     <div className={Styles.signup}>
       <LoginHeader/>
-      <Context.Provider value={{ state: {} }}>
+      <Context.Provider value={{ state, setState }}>
         <form className={Styles.form}>
           <h2>Criar Conta</h2>
           <Input type="text" name="name" placeholder="Digite seu nome"/>
           <Input type="email" name="email" placeholder="Digite seu e-mail"/>
           <Input type="password" name="password" placeholder="Digite sua senha"/>
           <Input type="password" name="passwordConfirmation" placeholder="Confirme sua senha"/>
-          <button className={Styles.submit} type="submit">Entrar</button>
-          <Link to="/login" className={Styles.link}>Acessar conta</Link>
+          <button data-testid="submit" disabled className={Styles.submit} type="submit">Entrar</button>
+          <span className={Styles.link}>Acessar conta</span>
           <FormStatus/>
         </form>
       </Context.Provider>
