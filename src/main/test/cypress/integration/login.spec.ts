aef8a43ad239e262a47c8e0d25787cb3d0ddb404
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-14 17:44:10 -0300

    test: ensure Login present valid state if values are valid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -30,4 +30,17 @@ describe('Login', () => {
     cy.getByTestId('submit').should('have.attr', 'disabled', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
+
+  it('should present valid state if form is valid', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('have.text', '🔵')
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('have.text', '🔵')
+    cy.getByTestId('submit').should('not.have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
 })
