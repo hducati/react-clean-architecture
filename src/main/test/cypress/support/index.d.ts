073b4063049db7bd59a2b6ab5250d973d1ef00cd
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-15 16:57:43 -0300

    feat: add cypress command to mock requests

--- src/main/test/cypress/support/index.d.ts
+++ src/main/test/cypress/support/index.d.ts
@@ -1,5 +1,6 @@
 declare namespace Cypress {
   interface Chainable {
     getByTestId: (id: string) => Chainable<Element>
+    mockRequest: (method, url, statusCode, body) => Chainable<Element>
   }
 }
--- src/main/test/cypress/support/index.js
+++ src/main/test/cypress/support/index.js
@@ -1,1 +1,11 @@
 Cypress.Commands.add('getByTestId', (id) => cy.get(`[data-testid=${id}]`))
+Cypress.Commands.add('mockRequest', (method, url, statusCode, body) =>
+  cy.intercept({
+    method: method,
+    url: url
+  }, {
+    statusCode: statusCode,
+    body: {
+      body
+    }
+  }))
