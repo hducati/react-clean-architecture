5ca3ebc7075eb25e745c1cfc1e033e272a922851
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-17 08:59:40 -0300

    test: ensure Input has focus when label is clicked

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -30,4 +30,15 @@ describe('Input Component', () => {
 
     expect(input.readOnly).toBe(false)
   })
+
+  test('should focus input on label click', () => {
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const input = sut.getByTestId(field)
+    const label = sut.getByTestId(`${field}-label`)
+
+    fireEvent.click(label)
+
+    expect(document.activeElement).toBe(input)
+  })
 })
