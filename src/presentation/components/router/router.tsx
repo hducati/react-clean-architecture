1463686c1a4a1130a591ffe95e086014e3b8068a
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-18 11:09:10 -0300

    feat: add initial survey-list view

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { SurveyList } from '@/presentation/pages'
 import React from 'react'
 
 type Factory = {
@@ -12,6 +13,7 @@ const Router: React.FC<Factory> = (factory: Factory) => {
       <Switch>
         <Route path="/login" exact component={factory.makeLogin}/>
         <Route path="/signup" exact component={factory.makeSignUp}/>
+        <Route path="/" exact component={SurveyList}/>
       </Switch>
     </BrowserRouter>
   )
---
+++ src/presentation/pages/survey/survey-list.tsx
@@ -0,0 +1,72 @@
+import { Footer, Logo } from '@/presentation/components'
+import React from 'react'
+import Styles from './survey-list-styles.scss'
+
+const SurveyList: React.FC = () => {
+  return (
+    <div className={Styles.surveyListWrap}>
+      <header className={Styles.headerWrap}>
+        <div className={Styles.headerContent}>
+          <Logo/>
+          <div className={Styles.logoutWrap}>
+            <span>Henrique</span>
+            <a href="#">Sair</a>
+          </div>
+        </div>
+      </header>
+
+      <div className={Styles.contentWrap}>
+        <h2>Enquetes</h2>
+        <ul>
+          <li>
+            <div className={Styles.surveyContent}>
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+        </ul>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default SurveyList
