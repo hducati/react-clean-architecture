2de5e7026f705cc77268f799de688ebbf519abf8
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-20 17:58:58 -0300

    refactor: move subject creation to a factory method

--- src/presentation/pages/survey/survey-list.spec.tsx
+++ src/presentation/pages/survey/survey-list.spec.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { SurveyList } from '@/presentation/pages'
 
+const makeSubject = (): void => {
+  render(
+    <SurveyList />
+  )
+}
+
 describe('SurveyList Component', () => {
   test('should present four empty items on start', () => {
-    render(
-      <SurveyList />
-    )
+    makeSubject()
+
     const surveyList = screen.getByTestId('survey-list')
 
     expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
