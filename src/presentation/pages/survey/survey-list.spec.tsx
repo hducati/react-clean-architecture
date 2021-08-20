7eb28beb2cd9a5dfa7697a5972d42f0609200ead
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-20 17:34:00 -0300

    test: ensure SurveyList renders 4 empty LIs on start

---
+++ src/presentation/pages/survey/survey-list.spec.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { SurveyList } from '@/presentation/pages'
+
+describe('SurveyList Component', () => {
+  test('should present four empty items on start', () => {
+    render(
+      <SurveyList />
+    )
+    const surveyList = screen.getByTestId('survey-list')
+
+    expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+  })
+})
