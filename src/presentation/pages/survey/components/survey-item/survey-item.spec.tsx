487d86a5e7a7cf8bcc2383faa33526f35bad60c8
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-20 19:42:12 -0300

    refactor: move subject to a factory method

--- src/presentation/pages/survey/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey/components/survey-item/survey-item.spec.tsx
@@ -4,13 +4,17 @@ import { SurveyItem } from '@/presentation/pages/survey/components'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 
+const makeSubject = (survey = mockSurveyModel()): void => {
+  render(<SurveyItem survey={survey}/>)
+}
+
 describe('SurveyItem Component', () => {
   test('should render with correct values', () => {
-    const survey = mockSurveyModel()
-    survey.didAnswer = true
-    survey.date = new Date('2020-01-10T00:00:00')
-
-    render(<SurveyItem survey={survey}/>)
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: true,
+      date: new Date('2020-01-10T00:00:00')
+    })
+    makeSubject(survey)
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
