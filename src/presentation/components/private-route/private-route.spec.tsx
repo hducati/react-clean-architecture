e7a133d26e33de2ba660ec372ced1e72876ccbfc
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-20 14:49:48 -0300

    refactor: move subject to a factory method

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { Router } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import PrivateRoute from '@/presentation/components/private-route/private-route'
 
-describe('PrivateRoute', () => {
-  test('should redirect to /login if token is empty', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] })
+type SubjectTypes = {
+  history: MemoryHistory
+}
 
-    render(
+const makeSubject = (): SubjectTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+
+  render(
       <Router history={history}>
         <PrivateRoute />
       </Router>
-    )
+  )
+
+  return { history }
+}
+
+describe('PrivateRoute', () => {
+  test('should redirect to /login if token is empty', () => {
+    const { history } = makeSubject()
     expect(history.location.pathname).toBe('/login')
   })
 })
