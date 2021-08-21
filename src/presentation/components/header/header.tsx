a2e1d86964e545d6b922fc48390ab75eaa3808d6
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-21 20:57:13 -0300

    feat: ensure logout works properly if undefined is provided

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import Styles from './header-styles.scss'
 import { Logo } from '@/presentation/components'
+import { ApiContext } from '@/presentation/contexts'
+import { useHistory } from 'react-router-dom'
+import React, { memo, useContext } from 'react'
+import Styles from './header-styles.scss'
 
 const Header: React.FC = () => {
+  const history = useHistory()
+  const { setCurrentAccount } = useContext(ApiContext)
+  const handleLogout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+    event.preventDefault()
+    setCurrentAccount(undefined)
+    history.replace('/login')
+  }
+
   return (
     <header className={Styles.headerWrap}>
       <div className={Styles.headerContent}>
         <Logo />
         <div className={Styles.logoutWrap}>
           <span>Henrique</span>
-          <a href="#">Sair</a>
+          <a data-testid="logout" href="#" onClick={handleLogout}>Sair</a>
         </div>
       </div>
     </header>
   )
 }
 
-export default Header
+export default memo(Header)
