f126e90d2ce4ad2e70a0a28bc5d59eb4ce3b33c1
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-21 10:41:17 -0300

    refactor: move subject creation to a factory method

--- src/main/decorators/authorize-http-get-client/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client/authorize-http-get-client-decorator.spec.ts
@@ -1,10 +1,32 @@
 import { mockGetRequest , GetStorageSpy } from '@/data/test'
 import { AuthorizeHttpGetClientDecorator } from '@/main/decorators'
 
+type SubjectTypes = {
+  subject: AuthorizeHttpGetClientDecorator
+  getStorageSpy: GetStorageSpy
+}
+
+const makeSubject = (): SubjectTypes => {
+  const getStorageSpy = new GetStorageSpy()
+  const subject = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+
+  return {
+    subject,
+    getStorageSpy
+  }
+}
+
 describe('AuthorizeHttpGetClientDecorator', () => {
   test('should call GetStorage with correct value', () => {
     const getStorageSpy = new GetStorageSpy()
     const subject = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+    subject.get(mockGetRequest())
+
+    expect(getStorageSpy.key).toBe('account')
+  })
+
+  test('should not add headers if getStorage is invalid', () => {
+    const { subject, getStorageSpy } = makeSubject()
     subject.get(mockGetRequest())
 
     expect(getStorageSpy.key).toBe('account')
