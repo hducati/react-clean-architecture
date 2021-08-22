fc2df65481ffb6edbdcebd79661c627a1a6b5f54
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-22 10:50:36 -0300

    feat: ensure RemoteLoadSurveyList throws AccessDeniedError on 403

--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -1,3 +1,4 @@
 export * from './login/invalid-credentials-error'
 export * from './common/unexpected-error'
+export * from './common/access-denied-error'
 export * from './signup/email-in-use-error'
