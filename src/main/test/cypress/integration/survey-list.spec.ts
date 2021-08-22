fa60191d7e8a1e618134a99cb8f79a3d82d83871
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-22 12:43:13 -0300

    test: ensure SurveyList present error on UnexpectedError

---
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -0,0 +1,18 @@
+import * as Http from '../support/survey/survey-list-mocks'
+import * as Helper from '../support/helpers'
+import faker from 'faker'
+
+describe('SurveyList', () => {
+  beforeEach(() => {
+    Helper.setLocalStorageItem('account', {
+      accessToken: faker.datatype.uuid(),
+      name: faker.name.findName()
+    })
+    cy.visit('')
+  })
+
+  it('should present error on UnexpectedError', () => {
+    Http.mockUnexpectedError()
+    cy.getByTestId('error').should('contain.text', 'Something went wrong. Please try again')
+  })
+})
---
+++ src/main/test/cypress/support/survey/survey-list-mocks.ts
@@ -0,0 +1,8 @@
+import faker from 'faker'
+import * as Http from '../http-mocks'
+
+export const mockInvalidCredentialsError = (): void => Http.mockUnauthorizedError(/login/)
+export const mockUnexpectedError = (): void => Http.mockServerError(/surveys/, 'GET')
+export const mockOk = (): void => Http.mockOk('POST', /login/, {
+  accessToken: faker.datatype.uuid(), name: faker.name.firstName()
+})
