f1d8d6f0f68a302a45ddf478c83f5d1c8f08a6ba
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-26 16:40:28 -0300

    refactor: move subject creation to a factory method

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -1,12 +1,26 @@
 import { HttpGetClientSpy } from '@/data/test'
-import faker from 'faker'
 import { RemoteLoadSurveyResult } from '@/data/usecases'
+import faker from 'faker'
+
+type SubjectTypes = {
+  subject: RemoteLoadSurveyResult
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSubject = (url = faker.internet.url()): SubjectTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const subject = new RemoteLoadSurveyResult(url, httpGetClientSpy)
+
+  return {
+    subject,
+    httpGetClientSpy
+  }
+}
 
 describe('RemoteLoadSurveyResult', () => {
   test('should call HttpGetClient with correct URL', async () => {
     const url = faker.internet.url()
-    const httpGetClientSpy = new HttpGetClientSpy()
-    const subject = new RemoteLoadSurveyResult(url, httpGetClientSpy)
+    const { subject, httpGetClientSpy } = makeSubject(url)
     await subject.load()
 
     expect(httpGetClientSpy.url).toBe(url)
