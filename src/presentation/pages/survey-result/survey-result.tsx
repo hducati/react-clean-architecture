f0725ff658c51870a73bc331ec6d066ff998d046
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-26 21:40:03 -0300

    feat: add SurveyResult layout

---
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -0,0 +1,42 @@
+import { Footer, Header, Spinner } from '@/presentation/components'
+import FlipMove from 'react-flip-move'
+import React from 'react'
+import Styles from './survey-result-styles.scss'
+
+const SurveyResult: React.FC = () => {
+  return (
+    <div className={Styles.surveyResultWrap}>
+      <Header/>
+      <div className={Styles.contentWrap}>
+        <h2>Qual é seu framework web favorito?</h2>
+        <FlipMove className={Styles.answersList}>
+          <li>
+            <img src=""/>
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+          <li className={Styles.active}>
+            <img src=""/>
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+          <li>
+            <img src=""/>
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+        </FlipMove>
+        <button>Voltar</button>
+        <div className={Styles.loadingWrap}>
+          <div className={Styles.loading}>
+            <span>Aguarde..</span>
+            <Spinner isNegative />
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default SurveyResult
