a8577dd9d88526fbfb3dba604ba15e08c8f503b0
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-27 14:04:41 -0300

    test: ensure SurveyResult calls LoadSurveyResult

--- src/domain/test/index.ts
+++ src/domain/test/index.ts
@@ -1,4 +1,5 @@
 export * from './doubles/mock-authentication'
 export * from './doubles/mock-add-account'
 export * from './doubles/mock-survey-list'
+export * from './doubles/mock-survey-result'
 export * from './doubles/mock-account'
--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -1,24 +1,47 @@
-import { mockAccountModel } from '@/domain/test'
+import { LoadSurveyResultSpy, mockAccountModel } from '@/domain/test'
 import { ApiContext } from '@/presentation/contexts'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 import SurveyResult from './survey-result'
 
+type SubjectTypes = {
+  loadSurveyResultSpy: LoadSurveyResultSpy
+}
+
+const makeSubject = (): SubjectTypes => {
+  const loadSurveyResultSpy = new LoadSurveyResultSpy()
+
+  render(
+    <ApiContext.Provider value={{
+      setCurrentAccount: jest.fn(),
+      getCurrentAccount: () => mockAccountModel()
+    }}>
+        <SurveyResult loadSurveyResult={loadSurveyResultSpy} />
+    </ApiContext.Provider>
+  )
+
+  return {
+    loadSurveyResultSpy
+  }
+}
+
 describe('SurveyResult Component', () => {
-  test('should present correct initial state', () => {
-    render(
-      <ApiContext.Provider value={{
-        setCurrentAccount: jest.fn(),
-        getCurrentAccount: () => mockAccountModel()
-      }}>
-          <SurveyResult />
-      </ApiContext.Provider>
-    )
+  test('should present correct initial state', async () => {
+    makeSubject()
 
     const surveyResult = screen.getByTestId('survey-result')
 
     expect(surveyResult.childElementCount).toBe(0)
     expect(screen.queryByTestId('error')).not.toBeInTheDocument()
     expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+
+    await waitFor(() => surveyResult)
+  })
+
+  test('should call LoadSurveyResult', async () => {
+    const { loadSurveyResultSpy } = makeSubject()
+
+    await waitFor(() => screen.getByTestId('survey-result'))
+    expect(loadSurveyResultSpy.callsCount).toBe(1)
   })
 })
