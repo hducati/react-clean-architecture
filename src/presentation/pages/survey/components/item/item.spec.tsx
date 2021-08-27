1b709255b6032c0b7f3a749c341f4f82f8a95db4
Henrique Ducati <henrique.dcti@gmail.com>
2021-08-27 09:02:10 -0300

    refactor: move Calendar to its own Component

--- src/presentation/pages/survey/components/item/item.spec.tsx
+++ src/presentation/pages/survey/components/item/item.spec.tsx
@@ -10,30 +10,18 @@ const makeSubject = (survey = mockSurveyModel()): void => {
 
 describe('SurveyItem Component', () => {
   test('should render with correct values', () => {
-    const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: true,
-      date: new Date('2020-01-10T00:00:00')
-    })
+    const survey = Object.assign(mockSurveyModel(), { didAnswer: true })
     makeSubject(survey)
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
-    expect(screen.getByTestId('day')).toHaveTextContent('10')
-    expect(screen.getByTestId('month')).toHaveTextContent('jan')
-    expect(screen.getByTestId('year')).toHaveTextContent('2020')
   })
 
   test('should render with correct values', () => {
-    const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: false,
-      date: new Date('2019-05-03T00:00:00')
-    })
+    const survey = Object.assign(mockSurveyModel(), { didAnswer: false })
     makeSubject(survey)
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
-    expect(screen.getByTestId('day')).toHaveTextContent('03')
-    expect(screen.getByTestId('month')).toHaveTextContent('mai')
-    expect(screen.getByTestId('year')).toHaveTextContent('2019')
   })
 })
